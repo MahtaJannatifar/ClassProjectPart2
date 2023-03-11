c5b0c1130e522ec431fc747452124958437ef61e
Mahta Jannatifar <jannatif@usc.edu>
2023-03-10 18:29:04 -0800

    test config1

--- run_test1.sh
+++ run_test1.sh
@@ -4,5 +4,5 @@ rm -rf ${OUT_DIR}
 SRC_DIR=src/test/Part1Test
 
 mkdir -p ${OUT_DIR}
-javac -d ${OUT_DIR} -cp ".:lib/*" ${SRC_DIR}/StatusCode.java ${SRC_DIR}/AttributeType.java ${SRC_DIR}/TableMetadata.java ${SRC_DIR}/TableManager.java ${SRC_DIR}/TableManagerImpl.java ${SRC_DIR}/TableManagerTest.java
+javac -d ${OUT_DIR} -cp ".:lib/*" ${SRC_DIR}/StatusCode.java ${SRC_DIR}/AttributeType.java ${SRC_DIR}/TableMetadata.java ${SRC_DIR}/TableManager.java ${SRC_DIR}/TableManagerImpl.java ${SRC_DIR}/test/Part1Test.java
 java -cp "${OUT_DIR}:./lib/*" org.junit.runner.JUnitCore TableManagerTest
