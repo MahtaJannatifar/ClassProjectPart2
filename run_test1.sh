d1a7eea9761c0898cbb49427c45d9324b0b4c686
Mahta Jannatifar <jannatif@usc.edu>
2023-03-10 18:45:10 -0800

    test config1

--- run_test1.sh
+++ run_test1.sh
@@ -4,5 +4,5 @@ rm -rf ${OUT_DIR}
 SRC_DIR=src
 
 mkdir -p ${OUT_DIR}
-javac -d ${OUT_DIR} -cp ".:lib/*" ${SRC_DIR}/*
+javac -d ${OUT_DIR} -cp ".:lib/*" ${SRC_DIR}/test/Part1Test
 java -cp "${OUT_DIR}:./lib/*" org.junit.runner.JUnitCore ${SRC_DIR}/test/Part1Test
