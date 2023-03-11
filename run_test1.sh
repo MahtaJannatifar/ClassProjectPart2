605cf37c601fd7fca0afe883ed985f4ec758a1b4
Mahta Jannatifar <jannatif@usc.edu>
2023-03-10 18:27:59 -0800

    test config1

--- run_test1.sh
+++ run_test1.sh
@@ -1,7 +1,7 @@
 OUT_DIR=out
 rm -rf ${OUT_DIR}
 
-SRC_DIR=src/test1
+SRC_DIR=src/test/Part1Test
 
 mkdir -p ${OUT_DIR}
 javac -d ${OUT_DIR} -cp ".:lib/*" ${SRC_DIR}/StatusCode.java ${SRC_DIR}/AttributeType.java ${SRC_DIR}/TableMetadata.java ${SRC_DIR}/TableManager.java ${SRC_DIR}/TableManagerImpl.java ${SRC_DIR}/TableManagerTest.java
