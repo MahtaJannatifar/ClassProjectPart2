0118187343a73598894d7403dd5d3af7de5eb27c
Mahta Jannatifar <jannatif@usc.edu>
2023-03-10 18:38:53 -0800

    test config1

--- run_test1.sh
+++ run_test1.sh
@@ -1,8 +1,8 @@
 OUT_DIR=out
 rm -rf ${OUT_DIR}
 
-SRC_DIR=src/test/Part1Test
+SRC_DIR=src
 
 mkdir -p ${OUT_DIR}
-javac -d ${OUT_DIR} -cp ".:lib/*" ${SRC_DIR}/StatusCode.java ${SRC_DIR}/AttributeType.java ${SRC_DIR}/TableMetadata.java ${SRC_DIR}/TableManager.java ${SRC_DIR}/TableManagerImpl.java ${SRC_DIR}/test/Part1Test.java
-java -cp "${OUT_DIR}:./lib/*" org.junit.runner.JUnitCore Part1Test
+javac -d ${OUT_DIR} -cp ".:lib/*" ${SRC_DIR}/*
+java -cp "${OUT_DIR}:./lib/*" org.junit.runner.JUnitCore SRC_DIR/test/Part1Test
